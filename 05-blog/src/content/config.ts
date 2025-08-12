626bcc1d46b1495882085a96d717485b3bc63afd
Kratos1700 <hunterstarsa@hotmail.com>
2025-08-13 01:02:11 +0200

    Implement code changes to enhance functionality and improve performance across various modules.

--- 05-blog/src/content/config.ts
+++ 05-blog/src/content/config.ts
@@ -7,11 +7,15 @@ import { defineCollection, z } from "astro:content";
 // y sus tipos de datos utilizando zod para validación
 const blogCollection = defineCollection({
     type: "content",
-    schema: z.object({
+    schema: ({image})=> z.object({
         title: z.string(),
-        description: z.string(),
         date: z.date(),
-        image: z.string(),  
+        description: z.string(),
+        image: image(), 
+        // Aquí puedes agregar validaciones adicionales para la imagen pero no funciona
+      //   image: image().refine((img) => img.width < 1200, {
+        //    message: 'Image width must be less than 1200px',
+        //}),  
         author:z.string(),
         tags: z.array(z.string()),
         url: z.string().optional(), // opcional si no se quiere mostrar el enlace
